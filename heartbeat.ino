df6d72187d62a47d4636e9f6042a3d0fd8c4001a
Andy <mazhiyuannn@gmail.com>
2023-02-11 10:36:28 -0500

    made heartbeat more accurate

--- heartbeat.ino
+++ heartbeat.ino
@@ -1,80 +1,85 @@
-#define samp_siz 4 
-#define rise_threshold 4
-// Pulse Monitor Test Script 
-int sensorPin = 0; 
-void setup() 
-{ 
-  Serial.begin(9600); 
-} 
 
-void loop () 
-{ 
-  float reads[samp_siz], sum; 
-  long int now, ptr; 
-  float last, reader, start; 
-  float first, second, third, before, print_value; 
-  bool rising; 
-  int rise_count; 
-  int n; 
-  long int last_beat; 
-  for (int i = 0; i < samp_siz; i++) 
-    reads[i] = 0; 
-  sum = 0; 
-  ptr = 0; 
-  while(1) 
-  { 
-    // calculate an average of the sensor 
-    // during a 20 ms period (this will eliminate 
-    // the 50 Hz noise caused by electric light 
-    n = 0; 
-    start = millis(); 
-    reader = 0.; 
-    do 
-    { 
-      reader += analogRead(sensorPin); 
-      n++; 
-      now = millis(); 
-    }
-    while(now < start + 20); 
-    reader /= n;  
-    // we got an average 
-    // Add the newest measurement to an array 
-    // and subtract the oldest measurement from the array 
-    // to maintain a sum of last measurements 
-    sum -= reads[ptr]; 
-    sum += reader; 
-    reads[ptr] = reader; 
-    last = sum / samp_siz; 
-    // now last holds the average of the values in the array
-    // check for a rising curve (= a heart beat) 
-    if (last > before) 
-    { 
-      rise_count++; 
-      if (!rising && rise_count > rise_threshold) 
-      { 
-        // Ok, we have detected a rising curve, which implies a heartbeat. // Record the time since last beat, keep track of the two previous 
-        // times (first, second, third) to get a weighed average. 
-        // The rising flag prevents us from detecting the same rise 
-        // more than once. 
-        rising = true; 
-        first = millis() - last_beat; 
-        last_beat = millis(); 
-        // Calculate the weighed average of heartbeat rate 
-        // according to the three last beats 
-        print_value = 60000. / (0.4 * first + 0.3 * second + 0.3 * third); 
-        Serial.println(print_value); 
-        third = second; 
-        second = first; 
-      } 
-    } 
-    else 
-    { 
-      // Ok, the curve is falling 
-      rising = false; 
-      rise_count = 0; 
-    } 
-    before = last; 
-    ptr++; 
-    ptr %= samp_siz; 
-  } 
-} 
+#include "Arduino.h"
+
+//Using built in LED pin for demo
+#define ledPin 13
+
+// Pulse meter connected to any Analog pin
+#define sensorPin A0
+
+float alpha = 0.75;
+int period = 50;
+float max = 0.0;
+
+void setup() {
+
+	// Inbuilt LED
+	pinMode(ledPin, OUTPUT);
+
+	Serial.begin(9600);
+}
+
+void loop() {
+
+	// Arbitrary initial value for the sensor value (0 - 1023)
+	// too large and it takes a few seconds to 'lock on' to pulse
+	static float oldValue = 500;
+
+	// Time recording for BPM (beats per minute)
+	static unsigned long bpmMills = millis();
+	static int bpm = 0;
+
+	// Keep track of when we had the the last pulse - ignore
+	// further pulses if too soon (probably false reading)
+	static unsigned long timeBetweenBeats = millis();
+	int minDelayBetweenBeats = 400;
+
+	// This is generic code provided with the board:
+	// Read the sensor value (0 - 1023)
+	int rawValue = analogRead((unsigned char) sensorPin);
+
+	// Some maths (USA: math) to determine whether we are detected a peak (pulse)
+	float value = alpha * oldValue + (1 - alpha) * rawValue;
+	float change = value - oldValue;
+	oldValue = value;
+
+	// Forum suggested improvement (works very well)
+	// Display data on the LED via a blip:
+	// Empirically, if we detect a peak as being X% from
+	// absolute max, we find the pulse even when amplitude
+	// varies on the low side.
+
+	// if we find a new maximum value AND we haven't had a pulse lately
+	if ((change >= max) && (millis() > timeBetweenBeats + minDelayBetweenBeats)) {
+
+		// Reset max every time we find a new peak
+		max = change;
+
+		// Flash LED and beep the buzzer
+		digitalWrite(ledPin, 1);
+		tone(3, 2000, 50);
+
+		// Reset the heart beat time values
+		timeBetweenBeats = millis();
+		bpm++;
+	}
+	else {
+		// No pulse detected, ensure LED is off (may be off already)
+		digitalWrite(ledPin, 0);
+	}
+	// Slowly decay max for when sensor is moved around
+	// but decay must be slower than time needed to hit
+	// next pulse peak. Originally: 0.98
+	max = max * 0.97;
+
+	// Every 10 seconds extrapolate the pulse rate. Improvement would
+	// be to average out BPM over last 60 seconds
+	if (millis() >= bpmMills+10000) {
+		Serial.println(bpm * 6);
+		bpm = 0;
+		bpmMills = millis();
+	}
+
+	// Must delay here to give the value a chance to decay
+	delay(period);
+}
