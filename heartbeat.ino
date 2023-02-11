bf5940ca617eb4d1a9a38a6ed8a590fbec7f3bac
Andy <mazhiyuannn@gmail.com>
2023-02-11 13:49:33 -0500

    averages the bpm each 10 seconds

--- heartbeat.ino
+++ heartbeat.ino
@@ -7,79 +7,93 @@
 // Pulse meter connected to any Analog pin
 #define sensorPin A0
 
+int arr_bpm[10];
+
 float alpha = 0.75;
 int period = 50;
 float max = 0.0;
 
 void setup() {
 
-	// Inbuilt LED
-	pinMode(ledPin, OUTPUT);
+  // Inbuilt LED
+  pinMode(ledPin, OUTPUT);
 
-	Serial.begin(9600);
+  for (int j = 0; j < 10; j++) {
+    arr_bpm[j] = 0;
+  }
+
+  Serial.begin(9600);
 }
 
 void loop() {
 
-	// Arbitrary initial value for the sensor value (0 - 1023)
-	// too large and it takes a few seconds to 'lock on' to pulse
-	static float oldValue = 500;
+  // to find value between current and old
+  static float oldValue = 500;
 
-	// Time recording for BPM (beats per minute)
-	static unsigned long bpmMills = millis();
-	static int bpm = 0;
+  //trying to average the bpm each iteration
+  int averager = 1;
+  int index;
 
-	// Keep track of when we had the the last pulse - ignore
-	// further pulses if too soon (probably false reading)
-	static unsigned long timeBetweenBeats = millis();
-	int minDelayBetweenBeats = 400;
+  // Time recording for BPM (beats per minute)
+  static unsigned long bpmMills = millis();
+  static int bpm = 0;
+  static float avg_bpm = 0;
 
-	// This is generic code provided with the board:
-	// Read the sensor value (0 - 1023)
-	int rawValue = analogRead((unsigned char) sensorPin);
+  // to minimize bad input
+  static unsigned long timeBetweenBeats = millis();
+  int minDelayBetweenBeats = 400;
 
-	// Some maths (USA: math) to determine whether we are detected a peak (pulse)
-	float value = alpha * oldValue + (1 - alpha) * rawValue;
-	float change = value - oldValue;
-	oldValue = value;
+  // Read the sensor value (0 - 1023)
+  int rawValue = analogRead((unsigned char)sensorPin);
 
-	// Forum suggested improvement (works very well)
-	// Display data on the LED via a blip:
-	// Empirically, if we detect a peak as being X% from
-	// absolute max, we find the pulse even when amplitude
-	// varies on the low side.
+  // Some maths (USA: math) to determine whether we are detected a peak (pulse)
+  float value = alpha * oldValue + (1 - alpha) * rawValue;
+  float change = value - oldValue;
+  oldValue = value;
 
-	// if we find a new maximum value AND we haven't had a pulse lately
-	if ((change >= max) && (millis() > timeBetweenBeats + minDelayBetweenBeats)) {
+  // if we find a new maximum value AND we haven't had a pulse lately
+  if ((change >= max) && (millis() > timeBetweenBeats + minDelayBetweenBeats)) {
 
-		// Reset max every time we find a new peak
-		max = change;
+    // Reset max every time we find a new peak
+    max = change;
 
-		// Flash LED and beep the buzzer
-		digitalWrite(ledPin, 1);
-		tone(3, 2000, 50);
+    // Flash LED
+    digitalWrite(ledPin, 1);
+    tone(3, 2000, 50);
 
-		// Reset the heart beat time values
-		timeBetweenBeats = millis();
-		bpm++;
-	}
-	else {
-		// No pulse detected, ensure LED is off (may be off already)
-		digitalWrite(ledPin, 0);
-	}
-	// Slowly decay max for when sensor is moved around
-	// but decay must be slower than time needed to hit
-	// next pulse peak. Originally: 0.98
-	max = max * 0.97;
+    // Reset the heart beat time values
+    timeBetweenBeats = millis();
+    bpm++;
+  } else {
+    // No pulse detected, ensure LED is off (may be off already)
+    digitalWrite(ledPin, 0);
+  }
+  // Slowly decay max for when sensor is moved around
+  // but decay must be slower than time needed to hit
+  // next pulse peak. Originally: 0.98
+  max = max * 0.97;
 
-	// Every 10 seconds extrapolate the pulse rate. Improvement would
-	// be to average out BPM over last 60 seconds
-	if (millis() >= bpmMills+10000) {
-		Serial.println(bpm * 6);
+
+  if (index == 10)
+    index = 0;
+  if (averager >= 10)
+    averager = 10;
+  arr_bpm[index] = bpm;
+  for (int i = 0; i < 10; i++)
+  {
+    avg_bpm += arr_bpm[i];
+  }
+  avg_bpm = avg_bpm / averager;
+  averager++;
+  index++;
+  // Every 10 seconds extrapolate the pulse rate.
+  if (millis() >= bpmMills + 10000) {
+    Serial.println(avg_bpm * 6);
 		bpm = 0;
 		bpmMills = millis();
-	}
+  }
+  avg_bpm = 0;
 
-	// Must delay here to give the value a chance to decay
-	delay(period);
+  // Must delay here to give the value a chance to decay
+  delay(period);
 }
