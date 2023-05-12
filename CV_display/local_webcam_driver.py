c9e564f8b638eda067341b00e663a964202e758b
hepromark <markdo28102004@gmail.com>
2023-05-11 21:12:34 -0400

    finishing_touches

--- CV_display/local_webcam_driver.py
+++ CV_display/local_webcam_driver.py
@@ -11,16 +11,19 @@ class_labels=['Pain','Happy','Neutral','Sad','Surprise']
 
 def generate_frames(camera_frame):
     frame = np.copy(camera_frame)
-    labels=[]
-    gray=cv2.cvtColor(np.float32(frame),cv2.COLOR_BGR2GRAY) # Greyscales the frame
+    preds=[]
+    # Greyscale webcam input
+    gray=cv2.cvtColor(np.float32(frame),cv2.COLOR_BGR2GRAY) 
     gray = np.uint8(gray)
-    faces=face_classifier.detectMultiScale(gray,1.3,5) # Detects the face
+    # Localize face with Haarcascade
+    faces=face_classifier.detectMultiScale(gray,1.3,5) 
 
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray=gray[y:y+h,x:x+w]
         roi_gray=cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
+        # Feed face into emotion detector if face present
         if np.sum([roi_gray])!=0:
             roi=roi_gray.astype('float')/255.0
             roi= tf.keras.preprocessing.image.img_to_array(roi)
@@ -30,10 +33,8 @@ def generate_frames(camera_frame):
             label=class_labels[preds.argmax()]
             label_position=(x,y)
             cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
-        else:
-            cv2.putText(frame,'No Face Found',(20,20),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
-    
-    return frame
+
+    return frame, preds
 
 # Run webcam 
 if __name__ == "__main__":
@@ -41,7 +42,7 @@ if __name__ == "__main__":
     while True:
         ret, frame = camera.read()
         if frame.any() == True:
-            frame = generate_frames(frame)
+            frame, preds = generate_frames(frame)
             cv2.imshow('Emotion Detector',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
